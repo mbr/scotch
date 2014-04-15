4c0b414d6ac1f57f5c15aca5f6473be119b32771
Marc Brinkmann <git@marcbrinkmann.de>
2014-04-15 18:29:50 +0200

    First draft of somewhat working UWSGI configuration generation.

--- wsgideploy/plugins/uwsgi/__init__.py
+++ wsgideploy/plugins/uwsgi/__init__.py
@@ -8,7 +8,7 @@ class UWSGIPlugin(Plugin):
     def generate_uwsgi_config(self, app):
         output_fn = Path(app.config['uwsgi']['app_config'])
 
-        self.output_template('app.ini', output_fn)
+        self.output_template('app.ini', output_fn, config=app.config)
 
     def enable_app(self, app):
         WSGIApp.deployed_app.connect(self.generate_uwsgi_config)
